a946dcd12551b5b4a6045826b2749b23bc64f894
Adil Khan <adil_97@hotmail.it>
2026-07-09 13:57:55 +0100

    add countries region proxy route

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,32 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-const Quest = require('../models/quest');
-const Country = require('../models/country')
+const Country = require('../models/country');
 const verifyToken = require('../middleware/verify-token');
-const country = require('../models/country');
+
 
 router.get('/', verifyToken, async (req, res) => {
-    try {
-        const allCountries = await Country.find({})
-        // const countries = allCountries.filter(country => 
-        //     country.quests.length > 0
-        // )
-    res.status(200).json(allCountries)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+  try {
+    const allCountries = await Country.find({});
+    res.status(200).json(allCountries);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Get countries from Rest Countries API by region
+router.get('/region/:region', verifyToken, async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/region/${req.params.region}`
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 router.get('/:countryId', verifyToken, async (req, res) => {
-    try {
-        const country = await Country.findById(req.params.countryId)
-        .populate('quests')
-        res.status(200).json(country)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ err: error.message });
-    }
-})
+  try {
+    const country = await Country.findById(req.params.countryId)
+      .populate('quests');
 
-module.exports = router
+    res.status(200).json(country);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ err: error.message });
+  }
+});
+
+
+module.exports = router;
