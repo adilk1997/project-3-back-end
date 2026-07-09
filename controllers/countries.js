5dbe8e9bff248249aaded322ad4f0ca3d18db14d
Adil Khan <adil_97@hotmail.it>
2026-07-09 16:31:24 +0100

    use restcountries through backend proxy

--- controllers/countries.js
+++ controllers/countries.js
@@ -19,31 +19,12 @@ router.get('/', verifyToken, async (req, res) => {
 router.get('/region/:region', async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.api-ninjas.com/v1/country?region=${req.params.region}`,
-      {
-        headers: {
-          'X-Api-Key': process.env.COUNTRY_API_KEY
-        }
-      }
+      `https://restcountries.com/v3.1/region/${req.params.region}`
     );
 
     const data = await response.json();
 
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
-
-    const countries = data.map((country) => ({
-      name: {
-        common: country.name
-      },
-      cca3: country.iso2,
-      flags: {
-        png: `https://flagcdn.com/w320/${country.iso2.toLowerCase()}.png`
-      }
-    }));
-
-    res.status(200).json(countries);
+    res.status(200).json(data);
 
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -51,23 +32,13 @@ router.get('/region/:region', async (req, res) => {
 });
 
 
-// Get country by code
 router.get('/alpha/:code', async (req, res) => {
   try {
     const response = await fetch(
-      `https://api.api-ninjas.com/v1/country?name=${req.params.code}`,
-      {
-        headers: {
-          'X-Api-Key': process.env.COUNTRY_API_KEY
-        }
-      }
+      `https://restcountries.com/v3.1/alpha/${req.params.code}`
     );
 
     const data = await response.json();
-
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
 
     res.status(200).json(
       Array.isArray(data) ? data[0] : data
